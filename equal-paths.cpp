d40ecbd275b2686795d338ff6977f734f3177829
Anton Ramirez Padilla <antonram@usc.edu>
2022-11-04 23:38:43 +0000

    equal-paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -4,10 +4,57 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int findHeight(Node * root) {
+	//base case
+	if(root == NULL) {
+		return 0;
+	}
+
+	//calculate left and right heights
+	int leftHeight = 1 + findHeight(root->left);
+	int rightHeight = 1 + findHeight(root->right);
+
+	//return greater of the two
+	if(leftHeight > rightHeight) {
+		return leftHeight;
+	}
+	return rightHeight;
+
+}
+
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+		//base case
+		if(root == NULL) {
+			return true;
+		}
+
+		//if no children, then balanced
+		if(root->left == NULL && root->right == NULL) {
+			return true;
+		}
+
+		//if only left, check if left balanced
+		if(root->left != NULL && root->right == NULL) {
+			return equalPaths(root->left);
+		}
+
+		//if only right, check if right balanced
+		if(root->left == NULL && root->right != NULL) {
+			return equalPaths(root->right);
+		}
+
+		//if both, check if equal heights
+		if(findHeight(root->left) == findHeight(root->right)) {
+			return findHeight(root->left) && findHeight(root->right);
+		}
+
+		//if they were not equal, false
+		return false;
+		
+
 
 }
 
